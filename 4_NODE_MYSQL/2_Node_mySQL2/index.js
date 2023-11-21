48f2ffd2b9dfde61c8f199100f578a03de11ae21
EA - Murylo Oliveira Lopes <murylo_oliveira@educadventista.org>
2023-11-21 20:10:48 -0300

    21/11

--- 4_NODE_MYSQL/2_Node_mySQL2/index.js
+++ 4_NODE_MYSQL/2_Node_mySQL2/index.js
@@ -1,131 +1,155 @@
-const { response } = require("express")
-const express = require("express")
-const exphbs = require("express-handlebars")
-const mysql = require("mysql")
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql2");
 
-const app = express()
+const app = express();
 
-app.engine("handlebars", exphbs.engine())
-app.set("view engine", "handlebars")
+// Definindo o handlebars como template engine
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
 
-app.use(express.static("public"))
+// Pasta de arquivos estáticos como CSS, imagens...
+app.use(express.static("public"));
 
+// Trabalhar com dados no formato JSON
 app.use(express.urlencoded({
     extended: true
-}))
+}));
 
-app.use(express.json())
+// CRUD => CREATE, READ, UPDATE, DELETE
 
-app.post("/editsave", (req, res) => {
-    const {id, title, pageqty} = req.body
+app.use(express.json());
+
+// Rotas
+app.post("/delete", (req, res) => {
+    const { id } = req.body;
+
+    const sql = `
+        DELETE FROM books
+        WHERE id = ${id}
+    `;
+
+    conn.query(sql, (error) => {
+        if (error) {
+            return console.log(error);
+        }
+
+        res.redirect('/');
+    });
+});
+
+app.post("/edit/save", (req, res) => {
+    const { id, title, pageqty } = req.body;
 
     const sql = `
         UPDATE books
         SET title = '${title}', pageqty = '${pageqty}'
         WHERE id = ${id}
-    `
+    `;
+
     conn.query(sql, (error, data) => {
         if (error) {
-            return console.log(error)
+            return console.log(error);
         }
 
-        res.redirect('/')
-    })
-})
+        res.redirect('/');
+    });
+});
 
 app.post("/register/save", (req, res) => {
-    const { title, pageqty } = request.body
+    const { title, pageqty } = req.body;
 
     const query = `
         INSERT INTO books (title, pageqty)
-        VALUES ('${title}, ${pageqty}')
-    `
+        VALUES ('${title}', ${pageqty});
+    `;
 
     conn.query(query, (error) => {
         if (error) {
-            console.log(error)
-            return
+            console.log(error);
+            return;
         }
 
-        response.redirect("/")
-    })
-})
+        res.redirect('/');
+    });
+});
 
-app.get("edit/:id", (req, res) => {
-    const id = req.params.id
-    
-    const sql = `
-        SELEcT = FROM books
-        where id = ${id}
-    `
-
-    conn.query(sql, (error, data) => {
-        if (error) {
-            return console.log(error)
-        }
-
-            const book = data[0]
-
-            response.render('edit', {book})
-    })
-})
-
-app.get("/book/:id", (req, res) => {
-    const id = req.params.id 
+app.get("/edit/:id", (req, res) => {
+    const id = req.params.id;
 
     const sql = `
         SELECT * FROM books
-        WHERE id=${id}
-    `
+        WHERE id = ${id}
+    `;
 
     conn.query(sql, (error, data) => {
-        if(error) {
-            return console.log(error)
+        if (error) {
+            return console.log(error);
         }
 
-        const book = data[0]
+        const book = data[0];
 
-        response.render("book", { book })
-    })
-}) 
+        res.render("edit", { book });
+    });
+});
+
+app.get("/book/:id", (req, res) => {
+    const id = req.params.id;
+
+    const sql = `
+        SELECT * FROM books
+        WHERE id = ${id};
+    `;
+
+    conn.query(sql, (error, data) => {
+        if (error) {
+            return console.log(error);
+        }
+
+        const book = data[0];
+
+        res.render("book", { book });
+    });
+});
 
 app.get("/register", (req, res) => {
-    responde.render("register")
-})
+    res.render("register");
+});
 
-app.get("/", (req, res) => {
-    const query = 'SELECT * FROM BOOKS'
-    
+app.get('/', (req, res) => {
+    const sql = "SELECT * FROM books";
+
     conn.query(sql, (error, data) => {
-        if(error) {
-            return console.log(error)
+        if (error) {
+            return console.log(error);
         }
-        const books = data
 
-        console.log(books)
-        
-        response.render("home", { books })
-    })
-})
+        const books = data;
 
+        console.log(books);
+
+        res.render("home", { books });
+    });
+});
+
+// Conexão com MySQL
 const conn = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
     database: "nodemysql",
     port: 3306
-})
+});
 
 conn.connect((error) => {
     if (error) {
-        console.log(error)
-        return
+        console.log(error);
+        return;
     }
 
-    console.log("Conectado ao MySQL")
+    console.log("Conectado ao MySQL");
 
     app.listen(3000, () => {
-        console.log("Servidor rodando na porta 3000")
-    })
-})
-
+        console.log("Servidor rodando na porta 3000");
+    });
+});
